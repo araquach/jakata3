566e99f5014ef3835abc2adff2b2e73ecabbfe77
Araquach <adam@contra-design.com>
2013-02-14 12:17:08 +0000

    Changed create page

--- protected/views/valentine/create.php
+++ protected/views/valentine/create.php
@@ -15,10 +15,14 @@
 <?php else: ?>
 
 <h1>Valentines Competition</h1>
-<p>For your chance to win a set of <strong>'Gold' Edition GHD's</strong> or a <strong>FREE Blow Dry</strong>, simply fill out the form and answer the following question:</p>
-<p class="question">What do the letters 'GHD' stand for?</p>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<p class="comp_closed">The competition is now closed - look out for more soon!</p>
+
+<!--<p>For your chance to win a set of <strong>'Gold' Edition GHD's</strong> or a <strong>FREE Blow Dry</strong>, simply fill out the form and answer the following question:</p>
+<p class="question">What do the letters 'GHD' stand for?</p>-->
+
+<?php //echo $this->renderPartial('_form', array('model'=>$model)); ?>
+
 </div> <!--end #competition_form-->
 </section> <!--end #valentine-->
 
