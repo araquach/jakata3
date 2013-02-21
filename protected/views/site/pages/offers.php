039ec006e445b078f2d9dac3d13c34f833369d32
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-02-21 23:20:39 +0000

    Alterations to offers page

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -5,28 +5,31 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Ch
 
 <section id="offers">
 
+
 	<h1>Latest Offers</h1>
 
-	<section id="offer1" class="group">
-	<h2>FREE Colour with every Cut, Dry &amp; Style</h2>
-	<p>Have any Colour service absolutely FREE on your first visit to the salon
-	when you have a Cut, Dry &amp; Style</p>
-	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service Ends 28/02/13 </small>
-	</section> <!--end #offer1-->
+	<?php echo CHtml::link('<section id="offer1" class="group">
+	<h2>Catwalk Makeover <br>Package 1</h2>
+	<p>A limited edition package worth &pound;140 <br>for just &pound;70</p>
+	<p><strong>Click here for more details</strong></p>
+	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service Ends 31/03/13 </small>
+	(</section>', array('site/page','view'=>'catwalk')); ?>
 	
-	<section id="offer2">
+	<?php echo CHtml::link('<section id="offer2" class"group">
+	<h2>Catwalk Makeover <br>Package 2</h2>
+	<p>25% off a Cut, Dry &amp; Style plus FREE product and Catwalk Fashion Styling</p>
+	<p><strong>Click here for more details</strong></p>
+	<small>Excludes Saturday. First visit only. Not with any other offer. Ends 31/03/13</small>
+	
+	</section>', array('site/page','view'=>'catwalk2')); ?>
+	
+	
+	<section id="offer3" class="group">
 	<h2>30% Off Selected Products</h2>
 	<p>Come into the salon to see a wide selection of shampoo's, conditioners, treatments &amp; styling products</p>
 	<p>All with 30% off!</p>
 	<small>Subject to availability.</small>
-	</section><!--end #offer2-->
-	
-	<!--
-	<section id="offer4">
-	<h2>FREE Cut, Dry &amp; Style with every<br> <?php echo CHtml::link('Kebelo Treatment',array('kebelo/index')); ?></h2>
-	<p>The incredible new Brazilian blow dry treatment is only &pound;99 and includes a Cut, Dry &amp; Style plus aftercare products</p>
-	<small>Excludes Saturday. Not with any other offer. Ends 30/11/12</small>
-	</section><!--end #offer4-->
+	</section><!--end #offer3-->
 	
 	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
