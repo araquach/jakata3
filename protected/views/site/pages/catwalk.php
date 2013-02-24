2f98db46194f4b395ece70efd915bdb3403ea8ac
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-02-24 11:34:15 +0000

    Made a few minor changes

--- protected/views/site/pages/catwalk.php
+++ protected/views/site/pages/catwalk.php
@@ -11,13 +11,14 @@ $this->pageTitle=Yii::app()->name . ' - Catwalk Package - Hairdressers in Cheshi
 
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/catwalk/catwalk_logo.png" alt="Catwalk Package" width="316" height="100"/>
 	
-	<p>for a limited time</p>
-	<p>with a Senior Stylist</p>
+	<p class="cat_small">for a limited time only!</p>
+	<p><em>Colour &amp; Cut Package</em></p>
+	<p class="cat_small">with a Senior Stylist:</p>
 	<p class="cat_big"><strong>worth &pound;140</strong></p>
 	<p>for only &pound;70</p>
-	<p>with a Stylist</p> 
-	<p class="cat_med">only &pound;60</p>
-	<p class="cat_small">worth &pound;120</p>
+	<p class="cat_small"><em>the same package <br>with a Stylist:</em></p>
+	<p class="cat_med"><strong>only &pound;60</strong></p>
+	<p class="cat_small">worth &pound;120!</p>
 	<br>
 	<div id="cat_includes">
 	<p><em><strong>Package Includes:</strong></em></p>
@@ -25,7 +26,7 @@ $this->pageTitle=Yii::app()->name . ' - Catwalk Package - Hairdressers in Cheshi
 	<em>Catwalk Fashion Styling</em><br>
 	<em>FREE Catwalk Shampoo, Conditioner</em><br>
 	<em>&amp; Styling Product Worth £40</em></p>
-	<p class="cat_small">New clients only, not with any other offer, weekdays only. Ends 31/03/13</p>
+	<p class="cat_small">New clients only, not with any other offer. <br>Weekdays only. Ends 31/03/13</p>
 	</div> <!--#cat_includes-->
 	
 	<p class="link"><?php echo CHtml::link('Cut &amp Style only Package also available <br><strong>Click here for info</strong> ',array('site/page','view'=>'catwalk2')); ?></p>
--- protected/views/site/pages/catwalk2.php
+++ protected/views/site/pages/catwalk2.php
@@ -11,7 +11,8 @@ $this->pageTitle=Yii::app()->name . ' - Catwalk Package - Hairdressers in Cheshi
 
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/catwalk/catwalk_logo.png" alt="Catwalk Package" width="316" height="100"/>
 	
-	<p>for a limited time</p>
+	<p class="cat_small">for a limited time</p>
+	<p><em>Cut, Dry &amp; Style Only Package</em></p>
 	<p class="cat_big"><strong>25% off</strong></p>
 	<p class="cat_med">A Cut, Dry &amp; Style</p>
 	<p class="cat_big">PLUS</p>
@@ -19,9 +20,9 @@ $this->pageTitle=Yii::app()->name . ' - Catwalk Package - Hairdressers in Cheshi
 	<p><strong>PLUS</strong></p>
 	<p>FREE Catwalk product of your choice</p>
 	
-	<p class="cat_small">New clients only, not with any other offer, weekdays only. Ends 31/03/13</p>
+	<p class="cat_small">New clients only, not with any other offer. <br>Weekdays only. Ends 31/03/13</p>
 	
-	<p class="link"><?php echo CHtml::link('Cut &amp Colour Package also available <br><strong>Click here for info</strong> ',array('site/page','view'=>'catwalk')); ?></p>
+	<p class="link"><?php echo CHtml::link('Colour &amp Cut Package also available <br><strong>Click here for info</strong> ',array('site/page','view'=>'catwalk')); ?></p>
 	<p class="catwalk_contact">Call 01925 242960 to book</p>
 
 </section> <!--#catwalk-->
