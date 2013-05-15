38580da629782e322ac28e917e0019dbd34ad51f
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-05-15 10:28:44 +0100

    updated offers page with latest offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -2,43 +2,50 @@
 /* @var $this SiteController */
 $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Cheshire';
 ?>
-<!--
+
 <section id="offers">
 
 
 	<h1>Latest Offers</h1>
 
-	<?php echo CHtml::link('<section id="offer1" class="group">
+	<?php /*echo CHtml::link('<section id="offer1" class="group">
 	<h2>Catwalk Makeover <br>Colour &amp; Cut Package</h2>
 	<p>A limited edition package worth &pound;140 <br>for just &pound;70</p>
 	<p><strong>Click here for more details</strong></p>
 	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service Ends 26/04/13 </small>
-	</section>', array('site/page','view'=>'catwalk')); ?>
+	</section>', array('site/page','view'=>'catwalk')); */?>
 	
-	<?php echo CHtml::link('<section id="offer2" class"group">
+	<?php /*echo CHtml::link('<section id="offer2" class"group">
 	<h2>Catwalk Makeover <br>Cut &amp; Style Package</h2>
 	<p>A Cut, Dry &amp; Style plus FREE product and Catwalk Fashion Styling from only &pound;25</p>
 	<p><strong>Click here for more details</strong></p>
 	<small>Excludes Saturday. First visit only. Not with any other offer. Ends 26/04/13</small>
 	
-	</section>', array('site/page','view'=>'catwalk2')); ?>
+	</section>', array('site/page','view'=>'catwalk2')); */?>
 	
+	<section id="offer1" class="group">
+	<h2>&pound;50 Colour &amp; Cut package<br>for new clients</h2>
+	<p><strong>with a Senior Stylist</strong></p>
+	<p>Call 01925 242960 to take advantage of this limited offer!</p>
+	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service Ends 29/06/13 </small>
+	</section>
 	
-	<section id="offer3" class="group">
-	<h2></h2>
-	<p></p>
-	<p></p>
-	<small></small>
-	</section><!--end #offer3--*>
+	<section id="offer2" class"group">
+	<h2>&pound;30 Colour &amp; Cut Package<br>for new clients</h2>
+	<p><strong>with a Stylist/Graduate Stylist</strong></p>
+	<p>Call 01925 242960 to take advantage of this limited offer!</p>
+	<small>Excludes Saturday. First visit only. Not with any other offer. Ends 29/06/13</small>
+	</section>
+	
 	
 	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
 
-</section> <!--end #offers--*>-->
+</section> <!--end #offers-->
 
 
 
-<section id="no_offers">
+<!--<section id="no_offers">
 
 <h1>Latest Offers</h1>
 
@@ -47,4 +54,4 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Ch
 	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest offers </a>
 </p>
 
-</section> <!--no_offers-->
+</section> <!--no_offers--*>-->
