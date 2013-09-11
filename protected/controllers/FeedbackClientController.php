c6bb89d8df6826a430592db1658372765f6bccf8
Araquach <adam@contra-design.com>
2013-09-11 14:12:34 +0100

    added csv uploader

---
+++ protected/controllers/FeedbackClientController.php
@@ -0,0 +1,203 @@
+<?php
+
+class FeedbackController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/column2';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('create'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete','index','update','view'),
+				'users'=>array('manager'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+	
+	public function actionFeedbackVoucher ()
+	{
+		$this->render('//mail/feedback_voucher',array('model'=>$client));
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate($id)
+	{
+		
+		$client=$this->loadClient($id);
+		$model=new Feedback;
+		
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['Feedback']))
+		{
+			$model->attributes=$_POST['Feedback'];
+			$model->client_id = $client->id;
+			if($model->save())
+			{
+				$message = new YiiMailMessage;
+				$message->view = 'feedback_voucher';
+				$message->setBody(array('client'=>$client), 'text');
+				$message->subject = 'Jakata';
+				$message->addTo($client->mobile.'@smsid.textapp.net');
+				$message->from = ('enquiries@jakatasalon.co.uk');
+				
+				Yii::app()->mail->send($message);
+			
+				Yii::app()->user->setFlash('Feedback','Thank you for your feedback ' . ucfirst($client->first_name) . ', it\'s really appreciated.<br>Your voucher is on it\'s way plus you have been entered into our next prize draw for the chance to win some great prizes.<br>See you in the salon soon!');
+			}
+		
+		}
+
+		$this->render('create',array(
+			'model'=>$model,
+			'client'=>$this->loadClient($id),
+		));
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate($id)
+	{
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['Feedback']))
+		{
+			$model->attributes=$_POST['Feedback'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete($id)
+	{
+		$this->loadModel($id)->delete();
+
+		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+		if(!isset($_GET['ajax']))
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$criteria=new CDbCriteria();
+			$criteria->with = 'FeedbackClient';
+			$criteria->order = 't.id DESC';
+			
+		$dataProvider=new CActiveDataProvider('Feedback', array(
+			'criteria'=>$criteria));
+		
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$model=new Feedback('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['Feedback']))
+			$model->attributes=$_GET['Feedback'];
+
+		$this->render('admin',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer the ID of the model to be loaded
+	 */
+	public function loadModel($id)
+	{
+		$model=Feedback::model()->with('FeedbackClient')->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+	
+	public function loadClient($id)
+	{
+		$client=FeedbackClient::model()->with('Feedback')->findByPk($id);
+		if($client===null)
+			throw new CHttpException(404,'The requested client does not exist.');
+		return $client;
+	}
+	
+	/**
+	 * Performs the AJAX validation.
+	 * @param CModel the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='feedback-input-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
