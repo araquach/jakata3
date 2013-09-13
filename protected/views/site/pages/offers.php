e65ae37b53d35e3d46356cdc35edb3d0de6754bc
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-09-13 16:59:10 +0100

    new offers - need to update images though

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -2,7 +2,6 @@
 /* @var $this SiteController */
 $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Cheshire';
 ?>
-<!--
 <section id="offers">
 
 
@@ -24,43 +23,48 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Ch
 	</section>', array('site/page','view'=>'catwalk2')); */?>
 	
 	
-	<section id="offer1">
+	<!--<section id="offer1">
 	  <h2>FREE Colour <br>with every<br>Cut, Dry &amp; Style</h2>
 	  <p>For new clients<br>until the end of August</p>
 	    <small><time datetime="2013-08-30">until 30/08/13</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
-	</section>
+	</section>-->
 	
-	<?php echo CHtml::link('<section id="offer2">
+	<?php /* echo CHtml::link('<section id="offer2">
 	  <h2>FREE Cut Dry &amp; Style <br>with every<br>Kebelo Advantage Treatment</h2>
 	  <p>Throughout the month of August</p>
 	    <small>until <time datetime="2013-08-30">30/08/13</time><br>
 	    Not with any other offer. Excludes Saturday<br></small>
-	</section>', array('kebelo/index')); ?>
+	</section>', array('kebelo/index'));*/ ?>
 	
-	<!-<section id="offer1" class="group">
+	<section id="offer1" class="group">
 	<h2>&pound;50 Colour &amp; Cut package<br>for new clients</h2>
 	<p><strong>with a Senior Stylist</strong></p>
 	<p>Call 01925 242960 to take advantage of this limited offer!</p>
-	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service. Ends 29/06/13 </small>
+	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service.<br> Ends <time datetime="2013-10-13">29/10/13</time></small>
 	</section>
 	
 	<section id="offer2" class"group">
 	<h2>&pound;30 Colour &amp; Cut Package<br>for new clients</h2>
 	<p><strong>with a Stylist/Graduate Stylist</strong></p>
 	<p>Call 01925 242960 to take advantage of this limited offer!</p>
-	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service. Ends 29/06/13</small>
-	</section>-*>
+	<small>Excludes Saturday. First visit only. Not with any other offer. <br />Skin test required 48 hrs prior to service.<br> Ends <time datetime="2013-10-13">29/10/13</time></small>
+	</section>
+	
+	<section id="offer2" class"group">
+	<h2>&pound;10 Blow Dry<br>for new clients</h2>
+	<p><strong>Weekdays until 4.30pm</strong></p>
+	<p>Call 01925 242960 to book in!<br> (limited availability)</p>
+	<small>Excludes Saturday and evenings. First visit only. Not with any other offer. <br /> Ends <time datetime="2013-10-13">29/10/13</time></small>
+	</section>
 	
 	
 	<?php //echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
 
-</section> <!-end #offers -*>
--->
+</section> <!--end #offers-->
 
-
-<section id="no_offers">
+<!--<section id="no_offers">
 
 <h1>Latest Offers</h1>
 
@@ -69,4 +73,4 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Offers - Hairdressers in Ch
 	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest news and offers </a>
 </p>
 
-</section>
+</section>-->
