4fb4dedadf1a567487e52cc9b9106fea0175e614
Araquach <adam@contra-design.com>
2013-09-25 13:55:41 +0100

    updated exit page

--- protected/views/site/pages/exit.php
+++ protected/views/site/pages/exit.php
@@ -14,15 +14,15 @@ $this->pageTitle=Yii::app()->name . ' - Contact Us - hairdressers in Cheshire';
 
 <p>Last month Michelle (Shelly) decided to leave the salon to pursue a new opportunity in a totally different industry. We are sorry to see her go and wish her every success in her new career.</p>
 <p>If you were a regular client of Shelly's we apologise for her quick departure and we hope it hasn't caused you any inconvenience.</p>
-<p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, at a similar level to Shelly.  We have the talented <em>Jimmy Sharpe</em> who manages the salon plus we can also highly recommend <em>Michelle Ash</em> who is another senior stylist. Team leader <em>Vikki Rowland and Laura Hall</em> are also a similar level. </p>
+<p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, at a similar level to Shelly. We have fellow senior stylist <em>Maisie Thompson</em> plus we can highly recommend <em>Michelle Ash</em> who is another of our senior stylists. Both team leader <em>Vikki Rowland</em> and <em>Laura Hall</em> are also a similar level to Shelly.</p>
  <p><em>We would hate to lose you as a customer, we really appreciate your loyalty to the salon and we hope we can continue to look after you for the foreseeable future.</em> 
- <p>We have some fantastic offers as our way of apologising for the inconvenience&hellip;</em></p>
- <p class="offer">We're offering a <strong>25% off</strong> your next visit plus a <strong>free £10 </strong>product voucher.</p>
+ <p>We have a fantastic offer as our way of apologising for the inconvenience&hellip;</em></p>
+ <p class="exit-offer">We're offering <strong>25% off</strong> your next visit plus a <strong>free £10 </strong>product voucher.</p>
  
- <p>offers apply to Michelle, Vikki &amp; Laura for all previous regular clients to Shelly. Please mention the offer when booking and to your stylist.</p> <p class="small">(one offer per person, not transferable, ends 15/11/13)</p>
+ <p>Offer applies to Maisie, Michelle, Vikki &amp; Laura for all previous regular clients to Shelly. Please mention the offer when booking and to your stylist.</p> <p class="small">(one offer per person, not transferable, ends 15/11/13)</p>
  
  <p><em>Pictured right (top to bottom)</em></p>
- <p><em>Michelle Ash, Vikki Rowland &amp; Laura Hall</em></p>
+ <p><em>Maisie Thompson, Laura Hall, Vikki Rowland &amp; Michelle Ash</em></p>
 
 <p class="details_text"><strong>Call 01925 242960 to book your appointment</strong></p>
 
