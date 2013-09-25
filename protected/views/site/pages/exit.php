7dea26d762364241016b45269d90f628367b2af7
Araquach <adam@contra-design.com>
2013-09-25 12:59:53 +0100

    added exit page (Michelle leaving)

---
+++ protected/views/site/pages/exit.php
@@ -0,0 +1,31 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+
+$this->pageTitle=Yii::app()->name . ' - Contact Us - hairdressers in Cheshire';
+?>
+
+<section id="exit">
+
+<div id="exit_copy">
+
+<h1>Shelly French no longer at Jakata</h1>
+
+<p>Last month Michelle (Shelly) decided to leave the salon to pursue a new opportunity in a totally different industry. We are sorry to see her go and wish her every success in her new career.</p>
+<p>If you were a regular client of Shelly's we apologise for her quick departure and we hope it hasn't caused you any inconvenience.</p>
+<p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, at a similar level to Shelly.  We have the talented <em>Jimmy Sharpe</em> who manages the salon plus we can also highly recommend <em>Michelle Ash</em> who is another senior stylist. Team leader <em>Vikki Rowland and Laura Hall</em> are also a similar level. </p>
+ <p><em>We would hate to lose you as a customer, we really appreciate your loyalty to the salon and we hope we can continue to look after you for the foreseeable future.</em> 
+ <p>We have some fantastic offers as our way of apologising for the inconvenience&hellip;</em></p>
+ <p class="offer">We're offering a <strong>25% off</strong> your next visit plus a <strong>free £10 </strong>product voucher.</p>
+ 
+ <p>offers apply to Michelle, Vikki &amp; Laura for all previous regular clients to Shelly. Please mention the offer when booking and to your stylist.</p> <p class="small">(one offer per person, not transferable, ends 15/11/13)</p>
+ 
+ <p><em>Pictured right (top to bottom)</em></p>
+ <p><em>Michelle Ash, Vikki Rowland &amp; Laura Hall</em></p>
+
+<p class="details_text"><strong>Call 01925 242960 to book your appointment</strong></p>
+
+</div> <!--exit_copy-->
+</section> <!--end exit-->
+
