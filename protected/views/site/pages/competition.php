010782e86980fb5448cb576b167191750e52fe9b
Araquach <adam@contra-design.com>
2013-10-02 19:43:36 +0100

    keepin on truckin

--- protected/views/site/pages/competition.php
+++ protected/views/site/pages/competition.php
@@ -6,7 +6,6 @@ $this->pageTitle=Yii::app()->name . ' - Competitions - Hairdressers in Cheshire'
 
 <section id="competition">
 <div id="competition_copy">
-<h1>Competition</h1>
 <h2><strong>Look out for a great competition coming soon!</strong></h2>
 <a href="https://www.facebook.com/JakataSalon">Visit our Facebook page to keep up to date with the latest news, offers and competitions</a>
 <?php //echo CHtml::link('CLICK HERE to enter',array('valentine/create')); ?>
