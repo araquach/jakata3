c0b1347c432e604105ea2e7277877fac29dcc5a0
Araquach <adam@contra-design.com>
2013-10-04 17:56:42 +0100

    redesigned price layout

--- protected/views/site/pages/prices.php
+++ protected/views/site/pages/prices.php
@@ -57,121 +57,45 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Prices - Hairdressers in Ch
 
 <!--end mobile price list-->
 
-
-<div id="priceleft">
-<h3>Cutting &amp; Styling</h3>
-<p>Cut, Dry &amp; Style<br>
-Dry &amp; Style<br>
-Curly Dry &amp; Style<br>
-<strong>Hair Up Styling</strong><br>
-Express Hair Up<br>
-Regular Hair Up<br>
-Intricate Hair Up</p>
-<p>Men's Wash, Cut &amp; Style<br>
-  Men's Express Cut &amp; Style<br>
-  Boy's Cut &amp; Style (Under 12)<br>
-  Girls's Dry Cut (Under 12)<br>
-  Girl's Cut, Dry &amp; Style (under 12)</p>
-  <h3>Colouring</h3>
-  <p><strong>Highlights &amp; Lowlights:</strong><br>
-  Top/Parting<br>
-  Half Head<br>
-  Full Head</p>
-  <p><strong>Full Head tint</strong><br>
-  Short<br>
-  Long</p>
-  <p>Root Regrowth<br>
-  Root &amp; Refresh</p>
-  <H3>'Vibrance'<br>
-  Fresh, intense ammonia free colours</h3>
-  <p>Short<br>
-  Long</p>
-  <h3>Full Colour Change</h3>
-  <p>Light to Dark (inc. Pre-pigmentation)<br>
-  Dark to Light (inc. Pre-lightening/colour cleansing)<br>
-  <br>
-  <br>
-  </p>
-  
-</div>
-
-<div id="pricemiddle">
-<h4 class="price_stylist">Stylists</h4>
-<p>£21.95 - £48.95<br>
-£14.95 - £32.95<br>
-£19.95 - £37.95<br>
-  <br>
-  £7.95 - £16.95<br>
-£14.95 - £38.95<br>
-£21.95 - £48.95</p>
-<p>£13.50 - £28.50<br>
-  £9.50 - £20.50<br>
-  £9.50 - £20.50 <br>
-  £10.95 - £21.95<br>
-  £16.95 - £36.95</p>
-  <h4 class="price_stylist">Stylists</h4>
-  <p><br>
-  £25.95 - £42.95<br>
-  £41.95 - £53.95<br>
-  £48.95 - £61.95</p>
-  <p><br>
-  £30.95 - £43.95<br>
-  £40.95 - £55.95</p>
-  <p>£30.95 - £40.95<br>
-  £41.95 - £50.95</p>
-  <h4><br>
-    <br>
-  </h4>
-  <p>£26.95 - £38.95<br>
-  £30.95 - £56.95</p>
-  <h4>&nbsp;</h4>
-  <p>£35.95 - £60.95<br>
-  £40.95 - £80.95<br>
-  <br>
-  <br>
-  </p>
-</div>
-
-<div id="priceright" class="group">
-<h4 class="price_director">Directors</h4>
-<p>£51.95<br>
-£35.95<br>
-£39.95<br>
-<br>
-<br>
-<br>
-<br></p>
-<p>£30.50<br>
-  £21.50<br>
-  £21.50<br>
-  £22.95<br>
-  £37.95</p>
-  <h4 class="price_director">Directors</h4>
-  <p><br>
-  £50.95<br>
-  £57.95<br>
-  £66.95</p>
-  <p><br>
-  £44.95<br>
-  £59.95</p>
-  <p>£44.95<br>
-  £54.95</p>
-  <h4><br>
-   <br>
-  </h4>
-  <p>£42.95<br>
-  £59.95</p>
-  <h4>&nbsp;</h4>
-  <p>£68.95<br>
-  £90.95<br>
-  <br>
-  <br>
-  </p>
-</div>
-
-<div id="pricebottom" class="group">
+<div id="norm_price">
+<table>
+	<tr><th class="s_type">Cutting &amp; Styling</th> <th>&nbsp;</th> <th>&nbsp;</th></tr>
+	<tr><th>&nbsp;</th> <th>Stylists</th> <th>Director</th></tr>
+	<tr><td>Cut, Dry &amp; Style</td> <td>&pound;24.95 - &pound;48.95</td> <td>&pound;56.95</td></tr>
+	<tr><td>Dry &amp; Style</td> <td>&pound;16.95 - &pound;32.95</td> <td>&pound;37.80</td></tr>
+	<tr><td>Curly Blowdry</td> <td>&pound;19.95 - &pound;38.95</td> <td>&pound;43.60</td></tr>
+	<tr><td><strong>Hair Up Styling</strong></td></tr>
+	<tr><td>Express Hair Up</td> <td>&pound;9.95 - &pound;16.95</td></tr>
+	<tr><td>Regular Hair Up</td> <td>&pound;16.95 - &pound;38.95</td></tr>
+	<tr><td>Intricate Hair Up</td> <td>&pound;24.95 - &pound;48.95</td></tr>
+	<tr><td><strong>Men</strong></td></tr>
+	<tr><td>Men's Wash, Cut &amp; Style</td> <td>&pound;11.95 - &pound;28.50</td> <td>&pound;33.80</td></tr>
+	<tr><td>Men's Express Cut &amp; Style</td> <td>&pound;8.95 - &pound;20.70</td> <td>&pound;23.95</td></tr>
+	<tr><td>Men's Colour</td> <td>From &pound;10</td></tr>
+	<tr><td><strong>Kids</strong></td></tr>
+	<tr><td>Boy's Cut &amp; Style (Under 12)</td> <td>&pound;9.50 - &pound;20.50</td> <td>&pound;23.90</td></tr>
+	<tr><td>Girls's Dry Cut (Under 12)</td> <td>&pound;12.95 - &pound;21.80</td> <td>&pound;25.05</td></tr>
+	<tr><td>Girls's Cut, Dry &amp; Style (Under 12)</td> <td>&pound;16.95 - &pound;35.95</td> <td>&pound;37.95</td></tr>
+	<tr><th class="s_type">Colouring</th> <td>&nbsp;</td> <td>&nbsp;</td></tr>
+	<tr><td><strong>Highlights &amp; Lowlights</strong></td></tr>
+	<tr><td>Top/Parting</td> <td>&pound;25.95 - &pound;51.95</td> <td>&pound;??</td></tr>
+	<tr><td>Half Head</td> <td>&pound;41.95 - &pound;59.95</td> <td>&pound;??</td></tr>
+	<tr><td>Full Head</td> <td>&pound;48.95 - &pound;69.95</td> <td>&pound;??</td></tr>
+	<tr><td><strong>Full Head Tint</strong></td></tr>
+	<tr><td>Short</td> <td>&pound;30.95 - &pound;44.95</td> <td>&pound;??</td></tr>
+	<tr><td>Long</td> <td>&pound;40.95 - &pound;59.95</td> <td>&pound;??</td></tr>
+	<tr><td>Root Regrowth</td> <td>&pound;30.95 - &pound;48.95</td> <td>&pound;??</td></tr>
+	<tr><td>Root &amp; Refresh</td> <td>&pound;41.95 - &pound;56.95</td> <td>&pound;??</td></tr>
+	<tr><td><strong>'Vibrance'<br>Fresh, intense <br>ammomia free colours</strong></td></tr>
+	<tr><td>Short</td> <td>&pound;26.95 - &pound;44.95</td> <td>&pound;??</td></tr>
+	<tr><td>Long</td> <td>&pound;30.95 - &pound;61.95</td> <td>&pound;??</td></tr>
+	<tr><td><strong>Full Colour Change</strong></td></tr>
+	<tr><td>Light to Dark<br>(inc. pre-pigmentation)</td> <td>&pound;35.95 - &pound;72.95</td> <td>&pound;??</td></tr>
+	<tr><td>Dark to Light<br>(inc. colour cleansing)</td> <td>&pound;40.95 - &pound;90.95</td> <td>&pound;??</td></tr>
+</table>
 <h3>Corrective Colour</h3>
-  <p>If you've had a home hair colouring disaster, or a bad colour experience in another salon, we will correct &amp; transform your hair to how you wanted it in the first place! Our expert colour team will give you a price on consultation.</p>
+<p>If you've had a home hair colouring disaster, or a bad colour experience in another salon, we will correct &amp; transform your hair to how you wanted it in the first place! Our expert colour team will give you a price on consultation.</p>
+
 </div>
 
 </section> <!--end prices-->
