8f55be57606a3e4307d01abac9f3b4ef7bbcb020
Araquach <adam@contra-design.com>
2013-10-05 13:53:41 +0100

    blah

--- protected/views/site/pages/prices.php
+++ protected/views/site/pages/prices.php
@@ -16,42 +16,43 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Prices - Hairdressers in Ch
 <h3>Cutting &amp; Styling</h3>
 <p>Prices depend on level of stylist</p>
 <table>
-	<tr><td>Cut, Dry &amp; Style</td> <td>&pound;21.95 - &pound;55.95</td></tr>
-	<tr><td>Dry &amp; Style</td> <td>&pound;14.95 - &pound;35.95</td></tr>
-	<tr><td>Curly Blowdry</td> <td>&pound;19.95 - &pound;39.95</td></tr>
+	<tr><td>Cut, Dry &amp; Style</td> <td>&pound;21.95 - &pound;65.95</td></tr>
+	<tr><td>Dry &amp; Style</td> <td>&pound;14.95 - &pound;37.80</td></tr>
+	<tr><td>Curly Blowdry</td> <td>&pound;19.95 - &pound;43.60</td></tr>
 	<tr><td><strong>Hair Up Styling</strong></td></tr>
-	<tr><td>Express Hair Up</td> <td>&pound;7.95 - &pound;16.95</td></tr>
-	<tr><td>Regular Hair Up</td> <td>&pound;14.95 - &pound;38.95</td></tr>
-	<tr><td>Intricate Hair Up</td> <td>&pound;21.95 - &pound;48.95</td></tr>
+	<tr><td>Express Hair Up</td> <td>&pound;9.95 - &pound;16.95</td></tr>
+	<tr><td>Regular Hair Up</td> <td>&pound;16.95 - &pound;38.95</td></tr>
+	<tr><td>Intricate Hair Up</td> <td>&pound;24.95 - &pound;48.95</td></tr>
 	<tr><td><strong>Men</strong></td></tr>
-	<tr><td>Men's Wash, Cut &amp; Style</td> <td>&pound;13.50 - &pound;33.50</td></tr>
-	<tr><td>Men's Express Cut &amp; Style</td> <td>&pound;9.50 - &pound;23.50</td></tr>
+	<tr><td>Men's Wash, Cut &amp; Style</td> <td>&pound;11.95 - &pound;33.80</td></tr>
+	<tr><td>Men's Express Cut &amp; Style</td> <td>&pound;8.95 - &pound;23.95</td></tr>
 	<tr><td>Men's Colour</td> <td>From &pound;10</td></tr>
 	<tr><td><strong>Kids</strong></td></tr>
-	<tr><td>Boy's Cut &amp; Style (Under 12)</td> <td>&pound;9.50 - &pound;23.50</td></tr>
-	<tr><td>Girls's Dry Cut (Under 12)</td> <td>&pound;10.95 - &pound;24.95</td></tr>
-	<tr><td>Girls's Dry Cut (Under 12)</td> <td>&pound;16.95 - &pound;39.95</td></tr>
+	<tr><td>Boy's Cut &amp; Style (Under 12)</td> <td>&pound;9.50 - &pound;23.90</td></tr>
+	<tr><td>Girls's Dry Cut (Under 12)</td> <td>&pound;12.95 - &pound;25.05</td></tr>
+	<tr><td>Girls's Cut, Dry &amp; Style (Under 12)</td> <td>&pound;16.95 - &pound;37.95</td></tr>
 </table>
 <h3>Colouring</h3>
 <table>
 	<tr><td><strong>Highlights &amp; Lowlights</strong></td></tr>
-	<tr><td>Top/Parting</td> <td>&pound;25.95 - &pound;51.95</td></tr>
-	<tr><td>Half Head</td> <td>&pound;41.95 - &pound;59.95</td></tr>
-	<tr><td>Full Head</td> <td>&pound;48.95 - &pound;69.95</td></tr>
+	<tr><td>Top/Parting</td> <td>&pound;26.60 - &pound;48.30</td></tr>
+	<tr><td>Half Head</td> <td>&pound;34.95 - &pound;67.20</td></tr>
+	<tr><td>Full Head</td> <td>&pound;43.25 - &pound;86.10</td></tr>
 	<tr><td><strong>Full Head Tint</strong></td></tr>
-	<tr><td>Short</td> <td>&pound;30.95 - &pound;44.95</td></tr>
-	<tr><td>Long</td> <td>&pound;40.95 - &pound;59.95</td></tr>
-	<tr><td>Root Regrowth</td> <td>&pound;30.95 - &pound;48.95</td></tr>
-	<tr><td>Root &amp; Refresh</td> <td>&pound;41.95 - &pound;56.95</td></tr>
+	<tr><td>Short</td> <td>&pound;26.60 - &pound;48.30</td></tr>
+	<tr><td>Long</td> <td>&pound;43.25 - &pound;86.10</td></tr>
+	<tr><td>Root Regrowth</td> <td>&pound;26.60 - &pound;48.30</td></tr>
+	<tr><td>Root &amp; Refresh</td> <td>&pound;34.95 - &pound;67.20</td></tr>
 	<tr><td><strong>'Vibrance'<br>Fresh, intense <br>ammomia free colours</strong></td></tr>
-	<tr><td>Short</td> <td>&pound;26.95 - &pound;44.95</td></tr>
-	<tr><td>Long</td> <td>&pound;30.95 - &pound;61.95</td></tr>
+	<tr><td>Short</td> <td>&pound;26.60 - &pound;48.30</td></tr>
+	<tr><td>Long</td> <td>&pound;34.95 - &pound;67.20</td></tr>
 	<tr><td><strong>Full Colour Change</strong></td></tr>
-	<tr><td>Light to Dark<br>(inc. pre-pigmentation)</td> <td>&pound;35.95 - &pound;72.95</td></tr>
-	<tr><td>Dark to Light<br>(inc. colour cleansing)</td> <td>&pound;40.95 - &pound;90.95</td></tr>
+	<tr><td>Light to Dark<br>(inc. pre-pigmentation)</td> <td>&pound;43.25 - &pound;86.10</td></tr>
+	<tr><td>Dark to Light<br>(inc. colour cleansing)</td> <td>&pound;64.95 - &pound;129.15</td></tr>
 </table>
 <h3>Corrective Colour</h3>
 <p>If you've had a home hair colouring disaster, or a bad colour experience in another salon, we will correct &amp; transform your hair to how you wanted it in the first place! Our expert colour team will give you a price on consultation.</p>
+<p>Please note: A patch test is required 48 hours prior to any colour service if you have not had colour with us before. It only takes a couple of minutes to carry out.</p>
 
 </div>
 
@@ -82,7 +83,7 @@ $this->pageTitle=Yii::app()->name . ' - Hairdressing Prices - Hairdressers in Ch
 	<tr><td>Half Head</td> <td>&pound;34.95 - &pound;58.50</td> <td>&pound;67.20</td></tr>
 	<tr><td>Full Head</td> <td>&pound;43.25 - &pound;74.50</td> <td>&pound;86.10</td></tr>
 	<tr><td><strong>Full Head Tint</strong></td></tr>
-	<tr><td>Short</td> <td>&pound;34.95 - &pound;58.50</td> <td>&pound;67.20</td></tr>
+	<tr><td>Short</td> <td>&pound;34.95 - &pound;58.50</td> <td>&pound;48.30</td></tr>
 	<tr><td>Long</td> <td>&pound;43.25 - &pound;74.50</td> <td>&pound;86.10</td></tr>
 	<tr><td>Root Regrowth</td> <td>&pound;26.60 - &pound;41.95</td> <td>&pound;86.10</td></tr>
 	<tr><td>Root &amp; Refresh</td> <td>&pound;41.95 - &pound;56.95</td> <td>&pound;48.30</td></tr>
