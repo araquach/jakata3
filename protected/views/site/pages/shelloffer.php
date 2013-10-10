50a5fddba09bf1afd65adfcbfe0e2c2be0d3e2ab
Araquach <adam@contra-design.com>
2013-10-10 17:21:58 +0100

    added shell offer

---
+++ protected/views/site/pages/shelloffer.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this SiteController */
+$this->pageTitle=Yii::app()->name . ' - Special Offer - Hairdressers in Cheshire';
+?>
+
+<section id="specOffer_bground">
+	
+	<section id="specOffer">
+	
+	<h1>Michelle Ash back behind the chair</h1>
+	<p>Eighteen month's ago the Jakata team got the shocking news that their colleague and friend Michelle had been rushed into hospital with life threatening blood poisoning. </p>
+	<p>She was in a coma for weeks and weeks but managed to fight back. The road to full recovery has been a slow and steady one. Michelle started back in the salon 6 months ago doing reduced hours and taking on limited appointments.</p>
+	<p>She now feels ready to open up her column and start getting busier to eventually build her hours back to full time.</p>
+	<p><strong>Michelle would love to see you in the salon again so we've launched this fantastic offer:</strong></p>
+	
+	<p class="so_big"><strong>25% off</strong></p>
+	<p class="so_med"><strong>plus a FREE <br>&pound;10 product voucher</strong></p>
+	<p class="so_small">for all clients of Michelle</p>
+	
+	<p class="specOffer_contact"><strong>Call:<br>01925 242960</strong> to book</p>
+	<p class="so_small">ends 29/11/2013</p>
+	
+
+	</section> <!--#specOffer-->
+
+</section> <!--end #specOffer_bground-->
+
