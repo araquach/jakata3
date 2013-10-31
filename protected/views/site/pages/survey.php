8e738dcd101aea415dc3403a231c929e21309cd5
Adam Carter <adamcarter@jakatasalon.co.uk>
2013-10-31 08:00:41 +0000

    more subtle changes

--- protected/views/site/pages/survey.php
+++ protected/views/site/pages/survey.php
@@ -9,7 +9,7 @@
 <div id="survey_copy">
 <h1>The Great Jakata Autumn Giveaway</h1>
 
-<p><strong>This time of year we all need a bit of a pick-me-up, the cold weather is setting in fast and with Winter just around the corner it would be great to have some free pampering or maybe get some of the latest hair products handed to you for free.</strong></p>
+<p>This time of year we all need a bit of a pick-me-up, the cold weather is setting in fast and with Winter just around the corner it would be great to have some free pampering or maybe get some of the latest hair products handed to you for free.</p>
 <p><em>Over the next couple of months we're going to be giving away 100's of pounds worth of hair products and services that YOU can get your hands on!</em></p>
 
 <ul>
