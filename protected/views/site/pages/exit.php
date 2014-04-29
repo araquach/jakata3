c83c5355280c00dd60a6815a0eb36807c22e9a1b
Araquach <adam@contra-design.com>
2014-04-29 16:15:03 +0100

    updated exit page

--- protected/views/site/pages/exit.php
+++ protected/views/site/pages/exit.php
@@ -8,21 +8,20 @@
 
 <div id="exit_copy">
 
-<h1>Shelly French no longer at Jakata</h1>
+<h1>Mikala Sutcliffe no longer at the salon</h1>
 
-<p>Last month Michelle (Shelly) decided to leave the salon to pursue a new opportunity in a totally different industry. We are sorry to see her go and wish her every success in her new career.</p>
-<p>If you were a regular client of Shelly's we apologise for her quick departure and we hope it hasn't caused you any inconvenience.</p>
-<p>We're pleased to say that we have plenty of other stylists to choose from here in the salon, at a similar level to Shelly. We have fellow senior stylist <em>Maisie Thompson</em> plus we can highly recommend <em>Michelle Ash</em> who is another of our senior stylists. Both team leader <em>Vikki Rowland</em> and <em>Laura Hall</em> are also a similar level to Shelly.</p>
- <p><em>We would hate to lose you as a customer, we really appreciate your loyalty to the salon and we hope we can continue to look after you for the foreseeable future.</em> 
- <p>We have a fantastic offer as our way of apologising for the inconvenience&hellip;</em></p>
- <p class="exit-offer">We're offering <strong>25% off</strong> your next visit plus a <strong>free £10 </strong>product voucher.</p>
- 
- <p>Offer applies to Maisie, Michelle, Vikki &amp; Laura for all previous regular clients to Shelly. Please mention the offer when booking and to your stylist.</p> <p class="small">(one offer per person, not transferable, ends 15/11/13)</p>
+<p>At the end of April Mikala made the decision to move on from Jakata to pursue something new. We were sorry to see her go and wish her every success in her new career.</p>
+<p>If you were a regular client of Mikala's we apologise for her quick departure and we hope it hasn't caused you any inconvenience.</p>
+<p>We currently don't have any stylists at Mikala's price level here at Jakata, though we will shortly be recruiting a new graduate stylist. Our other Warrington based salon <strong>Paul Kemp Hairdressing</strong> has 2 stylists at a similar price range to Mikala - <em>Tash &amp; Meg (pictured)</em> who would love to look after you.</p>
+ <p><em>We would hate to lose you as a customer, we really appreciate your loyalty to the salon and we hope we will be seeing you soon.</em>
  
  <p><em>Pictured right (top to bottom)</em></p>
- <p><em>Maisie Thompson, Laura Hall, Vikki Rowland &amp; Michelle Ash</em></p>
+ <p><em>Tash &amp; Meg</em></p>
 
-<p class="details_text"><strong>Call 01925 242960 to book your appointment</strong></p>
+<p class="details_text"><strong>If you already have an appointment with Mikala or for more information call Jakata on 01925 242960</strong></p>
+<p class="details_text"><strong>To book your next appointment with Meg or Tash call 01925 444488 at Paul Kemp Hairdressing</strong></p>
+
+<a href="http://www.paulkemphairdressing.com">Visit the <strong>Paul Kemp Hairdressing</strong> site to find out more about the salon</a>
 
 </div> <!--exit_copy-->
 </section> <!--end exit-->
