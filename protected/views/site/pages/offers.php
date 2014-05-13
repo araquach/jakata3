5c47b6a1d35e8e9ad1833a0e992e261067a19250
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-05-13 15:06:06 +0100

    updated the offers for May

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -3,14 +3,35 @@
 $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 ?>
 
+<?php Yii::app()->facebook->ogTags['og:title'] = "Offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = ""; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/offers/.jpg" ; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/offers/.jpg" ; ?>
 
-<section id="no_offers">
+<section id="offers">
 
-<h1>Latest Offers</h1>
+	<section id="offer1">
+	  <h2>Colour &amp; Cut Package</h2>
+	  <p>for just &pound;60<br>with any of our Stylists</p>
+	    <small><time datetime="2014-05-30">until 30/05/14</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer2">
+	  <h2>30% off Men's Cut &amp; Style</h2>
+	  <p>for new clients<br>throughout May</p>
+	    <small><time datetime="2014-05-30">until 30/05/14</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday</small>
+	</section>
+	
+	
+	<?php  echo CHtml::link('<section id="offer3">
+	  <h2>FREE Cut Dry &amp; Style <br>with every<br>Kebelo Advantage Treatment</h2>
+	  <p>Throughout the month of May</p>
+	    <small>until <time datetime="2014-05-30">30/05/14</time><br>
+	    Not with any other offer. Excludes Saturday<br></small>
+	</section>', array('kebelo/index')); ?>
+	
+	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
-<p>Look out for some great new offers soon!</p>
-<p>
-	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest news and offers </a>
-</p>
-
-</section>
+</section> <!--end #offers-->
