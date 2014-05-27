85fc7bb9ef39293381804268bb638bf32939fa09
Araquach <adam@contra-design.com>
2014-05-27 14:00:11 +0100

    created jimmy fend news page

---
+++ protected/views/site/pages/jimmy.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this SiteController */
+$this->pageTitle=Yii::app()->name . 'Jimmy News';
+?>
+
+<?php Yii::app()->facebook->ogTags['og:title'] = "Jimmy on hand at Fendi Launch"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Jakata manager Jimmy Sharpe was featured in this month's Warrington Worldwide. Read the full article here..."; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/newspics/jim_model.jpg"; ?>
+
+<section id="jimmy_fendi">
+
+<div id="jimmy_fendi_copy">
+
+<h1>Jimmy featured in this month's Warrington Worldwide</h1>
+
+<p>This month Jakata stylist <em>Jimmy Sharpe</em> featured in <a href="http://www.warrington-worldwide.co.uk" target="_blank">Warrington Wordwide Magazine</p>
+<p>It's a feature about him doing the hairdressing at the recent Fendi store opening. This is the page lifted straight from the magazine...</p>
+
+<div id="fendi_article">
+
+</div>
+
+</div> <!--jimmy_fendi_copy-->
+</section> <!--end jimmy_fendi-->
+
