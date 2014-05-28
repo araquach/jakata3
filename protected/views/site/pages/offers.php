090c204fa5bb379bbb9e7091d59da161ceb621c2
Araquach <adam@contra-design.com>
2014-05-28 13:30:30 +0100

    updated offer date

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -12,14 +12,14 @@ $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 	<section id="offer1">
 	  <h2>Colour &amp; Cut Package</h2>
 	  <p>for just &pound;60<br>with any of our Stylists</p>
-	    <small><time datetime="2014-05-30">until 30/05/14</time><br>
+	    <small><time datetime="2014-06-27">extended until 27/06/14</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
 	<section id="offer2">
 	  <h2>30% off Men's Cut &amp; Style</h2>
 	  <p>for new clients<br>throughout May</p>
-	    <small><time datetime="2014-05-30">until 30/05/14</time><br>
+	    <small><time datetime="2014-06-27">extended until 27/06/14</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday</small>
 	</section>
 	
@@ -27,7 +27,7 @@ $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 	<?php  echo CHtml::link('<section id="offer3">
 	  <h2>FREE Cut Dry &amp; Style <br>with every<br>Kebelo Advantage Treatment</h2>
 	  <p>Throughout the month of May</p>
-	    <small>until <time datetime="2014-05-30">30/05/14</time><br>
+	    <small>extended until <time datetime="2014-06-27">27/06/14</time><br>
 	    Not with any other offer. Excludes Saturday<br></small>
 	</section>', array('kebelo/index')); ?>
 	
