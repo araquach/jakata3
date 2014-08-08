5603918214cde6e871067ca471ba463c9321ef49
Araquach <adam@contra-design.com>
2014-08-08 16:08:48 +0100

    reworking offers application

--- protected/views/offer/index.php
+++ protected/views/offer/index.php
@@ -2,7 +2,16 @@
 /* @var $this OfferController */
 ?>
 
+<?php if($model->offer != 6): ?>
+
 <div id="limited">
+
+<?php else: ?>
+
+<div id="limitedmale">
+
+<?php endif; ?>
+
 <?php if(Yii::app()->user->hasFlash('Offer')): ?>
 
 <div class="limitedSuccess">
@@ -11,19 +20,18 @@
 
 <?php else: ?>
 <div id="limited_copy">
-<h1><strong>Last minute booking offer for <?php echo ucfirst($model->first_name) . ' ' . ucfirst($model->last_name);?></strong></h1>
+<h1><strong>Special Offer for<br> <?php echo ucfirst($model->first_name) . ' ' . ucfirst($model->last_name);?></strong></h1>
 
 <p><strong>Hi <?php echo ucfirst($model->first_name); ?>,</strong></p>
 
-<p><strong>With Christmas nearly here we're almost fully booked! If you still haven't made your appointment yet, fear not - we might still be able to get you in.</strong></p>
-<p>This time of year we have a lot of last minute cancellations (which we obviously want to fill!)</p>
-<p><em>If you call the salon on the day and we have availability we'll do your hair at a very special price.</em></p>
-<p>We can't guarantee we'll have availability and you can't request a specific stylist, but your appointment will be with a fully qualified stylist/senior stylist&hellip;</p>
-<p class="big">30% off</p>
-<p class="small">all services</p>
-<p><strong>Just call 01925 242960 to book and mention the offer.</strong></p>
+<p>To help give your hair the care and attention it needs over summer we've put together a great offer for you.</p>
+
+<?php echo $model->getOfferText(); ?>
+
+<p>To get your offer code just click 'Go'</p>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+
 
 <small>Not with any other offer, subject to availability</small>
 
@@ -31,3 +39,6 @@
 </div> <!--#limited-->
 
 <?php endif; ?>
+
+
+
