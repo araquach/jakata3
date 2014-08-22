5d8556f795dc4e8630c5e89bf4412ff3ff116da4
Araquach <adam@contra-design.com>
2014-08-22 18:11:16 +0100

    added vikki hours info

---
+++ protected/views/site/pages/news_extra.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+?>
+
+<section id="news_extra">
+
+<div id="news_extra_copy">
+
+<h1>Vikki Rowland
+<br>Due back dates</h1>
+
+<p>Just to keep you updated with Vikki's return to the salon plan. She returns on <strong>Thursday 11th September</strong> where she'll only be working Thursdays &amp; Saturdays throughout September/October.</p>
+<p>By December her hours will increase, but she will remain part-time for the foreseeable future.</p>
+<p>Her new hours from December are:</p>
+<ul>
+	<li>Tue : OFF</li>
+	<li>Wed : 9am - 1pm</li>
+	<li>Thu : 4pm - 8pm</li>
+	<li>Fri : 10am - 2pm</li>
+	<li>Sat : 8am - 4.30pm</li>
+</ul>
+
+<p>Please refer to the calendar below to find out Vikki's specific working days over the next few month's.</p>
+
+<div id="viks_hours"></div> <!--calender-->
+ 
+ <p><strong>Vikki can't wait to catch up with you - book early to ensure you get an appointment - spaces are limited!</strong></p>
+
+<p class="details_text"><strong>call Jakata on 01925 242960</strong></p>
+
+</div> <!--exit_copy-->
+</section> <!--end exit-->
+
