fa35660110b74749fb2031248d92931bfecaf595
Araquach <adam@contra-design.com>
2014-10-03 10:20:58 +0100

    removed offers

---
+++ protected/views/site/pages/offers.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this SiteController */
+$this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
+?>
+
+
+<section id="no_offers">
+
+<h1>Latest Offers</h1>
+
+<p>Look out for some great new offers soon!</p>
+<p>
+	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest news and offers </a>
+</p>
+
+</section>
