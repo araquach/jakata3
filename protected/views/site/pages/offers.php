284787685cbd31ca64a5f911fbbb818c01218439
Araquach <adam@contra-design.com>
2014-10-31 16:01:13 +0000

    Started work on updating the offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -3,14 +3,28 @@
 $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 ?>
 
+<?php Yii::app()->facebook->ogTags['og:title'] = "Offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "November Offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/offers/.jpg" ; ?>
 
-<section id="no_offers">
+<section id="offers">
 
-<h1>Latest Offers</h1>
+	<section id="offer1">
+	  <h2>Senior Stylist Colour &amp; Cut Package</h2>
+	  <p>for just &pound;60<br>with any of our Senior Stylists</p>
+	    <small><time datetime="2014-11-30">until 30/11/14</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer2">
+	  <h2>Stylist Colour &amp; Cut Package</h2>
+	  <p>for just &pound;50<br>with any of our Stylists</p>
+	    <small><time datetime="2014-11-30">until 30/11/14</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<p>Call 01925 242960 to book</p>
+	
+	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
-<p>Look out for some great new offers soon!</p>
-<p>
-	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest news and offers </a>
-</p>
-
-</section>
+</section> <!--end #offers-->
