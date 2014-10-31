b2ce7539e7a3cdad753187f3da4a330b13c4cc7c
Araquach <adam@contra-design.com>
2014-10-31 17:16:52 +0000

    more work on offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -9,21 +9,30 @@ $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 
 <section id="offers">
 
+	<h1>November Offers</h1>
+
 	<section id="offer1">
 	  <h2>Senior Stylist Colour &amp; Cut Package</h2>
 	  <p>for just &pound;60<br>with any of our Senior Stylists</p>
-	    <small><time datetime="2014-11-30">until 30/11/14</time><br>
+	    <small><time datetime="2014-11-28">until 28/11/14</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
 	<section id="offer2">
 	  <h2>Stylist Colour &amp; Cut Package</h2>
 	  <p>for just &pound;50<br>with any of our Stylists</p>
-	    <small><time datetime="2014-11-30">until 30/11/14</time><br>
+	    <small><time datetime="2014-11-28">until 28/11/14</time><br>
 	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
 	</section>
 	
-	<p>Call 01925 242960 to book</p>
+	<section id="offer3">
+	  <h2>30% Off</h2>
+	  <p>Men's Cut &amp; Style<br>with any of our Stylists/Senior Stylists</p>
+	    <small><time datetime="2014-11-28">until 28/11/14</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<p class="contact">Call 01925 242960 to book</p>
 	
 	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
 
