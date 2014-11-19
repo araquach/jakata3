f0237001d4f7cc87f3286453467387722927ad13
Adam Carter <adamcarter@jakatasalon.co.uk>
2014-11-19 15:01:05 +0000

    created competition page

--- protected/views/site/pages/competition.php
+++ protected/views/site/pages/competition.php
@@ -3,20 +3,52 @@
 $this->pageTitle=Yii::app()->name . 'Competitions';
 ?>
 
-<?php Yii::app()->facebook->ogTags['og:title'] = "Jakata Competition"; ?>
-<?php Yii::app()->facebook->ogTags['og:description'] = "Enter our latest competition"; ?>
-<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/main/jakata_logo_meta.jpg"; ?>
+<?php Yii::app()->facebook->ogTags['og:title'] = "The Jakata Christmas Give-away"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Hundreds of poundsworth of prizes to be won! GHD Styling tools set, meal for 4 at Grill on the Square plus Colour & Cut package with product hamper!"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/main/.jpg"; ?>
 
 
 <section id="competition">
 <div id="competition_copy">
-<h2><strong>Look out for a great competition coming soon!</strong></h2>
-<a href="https://www.facebook.com/JakataSalon">Visit our Facebook page to keep up to date with the latest news, offers and competitions</a>
-<?php //echo CHtml::link('CLICK HERE to enter',array('valentine/create')); ?>
-
-
-
+<h2>The JAKATA Christmas Give-away</h2>
+<h3><em>Hundreds of pounds worth of prizes to be won!</em></h3>
 </div> <!--end competition_copy-->
+<div id="prizes">
+	<div class="prize">
+		<h3>Top Prize</h3>
+		<p>The ultimate GHD styling tool collection</p>
+		<ul>
+			<li>A Set of GHD Gold series Irons - worth £120</li>
+			<li>A GHD Air hairdryer - worth £99</li>
+			<li>A set of GHD Curve styling tongues - worth £110)</li>
+			<li>Plus a selection of GHD brushes, products and other goodies</li>
+		</ul>
+		<p>A prize worth over £400!!</p>
+	</div> <!--.prize-->
+	
+	<div class="prize">
+		<h3>Second Prize</h3>
+		<p>A meal for four at Grill on the Square</p>
+		<p>Grill on the Square is one of Warrington’s finest restaurants. You can expect amazing food, great cocktails and impeccable service.</p>
+		<p>The prize consists of: A 3 course meal complete with a bottle of Veuve Clicquot for a party of four people</p>
+		<p>Worth over £250!!!</p>
+	
+	</div> <!--.prize-->
+	
+	<div class="prize">
+		<h3>Third Prize</h3>
+		<p>Colour, Cut &amp; product package</p>
+		<p>A FREE Colour &amp; Cut with your chosen stylist plus a hamper of  salon professional products.</p>
+		<p>Worth over £100!!</p>
+	</div> <!--.prize-->
+
+</div> <!--#prizes-->
+
+<div id="prize_entry">
+<p>To enter simply purchase prize draw tickets from salon reception. Tickets are £1 each and you can buy as many as you like!</p>
+<p>For every Christmas Gift pack purchased you will also receive a FREE prize draw ticket.</p>
+<p>The winner will be drawn on Monday 22nd December - in time for you to pick up your prize before Christmas</p>
+</div> <!--prize_entry-->
 
 
 </section> <!--end competition-->
