691d7df4882de5b41ae6f279eabaa8ff2f4aac5c
Araquach <adam@contra-design.com>
2015-03-06 17:38:53 +0000

    started getting the styling down

---
+++ protected/views/prospect/_hide.php
@@ -0,0 +1,8 @@
+
+
+<section id="tohide">
+
+<p>How would you like to get some products tailored to your hair type sent out to you absolutely FREE?<br>
+A selection of shampoo’s, conditioners and styling products could be yours - plus a secret surprise gift!</p>
+
+</section>
--- protected/views/prospect/create.php
+++ protected/views/prospect/create.php
@@ -7,18 +7,19 @@
 
 <section id="prospect-info">
 
+<img src="" alt="big enticing image" width="500" height="200" />
+
 <p>Jakata is a fashion forward, award winning hairdressing salon in Warrington, Cheshire. With a team of eight talented hairdressers looking after you, you know you're in good hands.</p>
 <p>Based on Bridge street in Warrington Town Centre the salon is located perfectly to serve the whole of Warrington and surrounding areas.</p>
 <p>If you're looking for the most fashion forward hairdressing then look no further. Our hairdressing services will far outweigh your expectations plus you'll be blown away by our friendly, approachable staff.</p>
-
-<img src="" alt="big enticing image" width="500" height="200" />
 
 <p>The Jakata team comprises of eight skilled stylists, all from different hairdressing backgrounds, all displaying a diverse range of skills.</p>
 <p>Over the years Jakata has trained and developed a number of people to exceptional standards, whilst taking on already established stylists. This mix of talent creates a dynamic team with stylists well suited to anyone with a desire for a fashion forward look.</p>
 <p><em>We know it's difficult to find a stylist well suited to your needs so we'll help you find a stylist most suited for your needs</em></p>
 
+</section> <!--prospect-info-->
 
-</section>
+<section id="prospect-entry">
 
 <div id="prospect_form">
 
@@ -31,15 +32,15 @@
 <?php else: ?>
 
 
-<h2>Claim your FREE products today!</h2>
-<p>We're giving away FREE products especially selected for your hair type - plus a secret FREE gift</p>
-<p>Simply fill out the form below to claim yours!</p>
-
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
 
 </div> <!--end #prospect_form-->
 
+
+
 <?php echo CHtml::link('Click here to go to our website', array('site/index')); ?>
+
+</section> <!--end prospect-entry-->
 
 </section> <!--end #prospect-->
 
