62744e73d39a4617a33042b6d1bff448258a7588
Araquach <adam@contra-design.com>
2015-04-25 10:17:02 +0100

    updated the offers

--- protected/views/site/pages/offers.php
+++ protected/views/site/pages/offers.php
@@ -3,14 +3,51 @@
 $this->pageTitle=Yii::app()->name . 'Hairdressing Offers';
 ?>
 
+<?php Yii::app()->facebook->ogTags['og:title'] = "Spring Offers"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Take advantage of these great offers for new clients: Colour & Cut package for just £55 with a stylist or £75 with a senior stylist"; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/offers/alex_meta.jpg" ; ?>
 
-<section id="no_offers">
+<section id="offers">
 
-<h1>Latest Offers</h1>
+<h1>Spring Offers</h1>
 
-<p>Look out for some great new offers soon!</p>
-<p>
-	<a href="http://www.facebook.com/JakataSalon" target="_blank">Visit our Facebook page<br> to keep up to date with the latest news and offers </a>
-</p>
+	<section id="offer1">
+	  <h2>Senior Stylist Colour &amp; Cut Package</h2>
+	  <p>for just &pound;75<br>with any of our Senior Stylists</p>
+	    <small><time datetime="2015-05-29">until 29/05/15</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer2">
+	  <h2>Stylist Colour &amp; Cut Package</h2>
+	  <p>for just &pound;55<br>with any of our Stylists</p>
+	    <small><time datetime="2015-05-29">until 29/05/15</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer3">
+	  <h2>Graduate Stylist Colour &amp; Cut Package</h2>
+	  <p>for just &pound;40<br>with any of our Graduate Stylists</p>
+	    <small><time datetime="2015-05-29">until 29/05/15</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday<br>Skin test required 48hrs before any colour service</small>
+	</section>
+	
+	<section id="offer4">
+	  <h2>Men's Cut &amp; Style</h2>
+	  <p>for just &pound;14 with a Stylist<br>&pound;18 with a Senior Stylist</p>
+	    <small><time datetime="2015-05-29">until 29/05/15</time><br>
+	    For New Clients - not with any other offer. Excludes Saturday</small>
+	</section>
+	
+	<?php echo CHtml::link('<p id="book_button">Online enquiry and booking form</p>',array('site/contact')); ?>
+	
+	<div id="fb_like">
+	<?php  $this->widget('ext.yii-facebook-opengraph.plugins.LikeButton', array(
+	   'href' => 'http://www.jakatasalon.co.uk/offers', 
+	   'show_faces'=>false,
+	   'send' => true,
+	   'width' => '250px'
+	));  ?>
+	</div>
 
-</section>
+</section> <!--end #offers-->
