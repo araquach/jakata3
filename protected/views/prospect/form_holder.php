89c4e7e9d46dba5f3aeb6ea3388cad85364e07ad
Araquach <adam@contra-design.com>
2015-08-14 10:14:20 +0100

    cleaned up the layout
    moved common info to layout page

--- protected/views/prospect/form_holder.php
+++ protected/views/prospect/form_holder.php
@@ -11,23 +11,6 @@ $this->pageTitle=Yii::app()->name. ' Free Products';
 
 <section id="prospect">
 
-<section id="prospect-info">
-
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/prospect/prospect_main.jpg" width="380" height="300" alt="">
-
-<p><strong>Jakata is a fashion forward, award winning hairdressing salon in Warrington.</strong></p>
-<p>The team comprises of eight skilled stylists, each from different hairdressing backgrounds, all displaying a diverse range of skills.</p>
-<p>This mix of talent creates a dynamic salon with stylists well suited to anyone looking for a fashion forward look.</p>
-<p><em>You'll be blown away by how friendly and approachable each of our team members are, and the warm, welcoming environment Jakata offers.</em></p>
-<p><strong>You know you're in good hands at JAKATA.</strong></p>
-
-<section id="address_info">
-<p>90/92 Bridge Street, Warrington</p>
-<p class="big">01925 242960</p>
-</section> <!--address_info-->
-
-</section> <!--prospect-info-->
-
 <section id="prospect-entry">
 
 <div id="prospect_form">
