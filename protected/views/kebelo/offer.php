b5676f46a8f51a325fb7e48df3ce7a2b1b1f4ebb
Araquach <adam@contra-design.com>
2015-10-10 16:45:23 +0100

    updated the kebelo pages

---
+++ protected/views/kebelo/offer.php
@@ -0,0 +1,34 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Kebelo Advantage';
+?>
+<?php Yii::app()->facebook->ogTags['og:title'] = "Kebelo at Jakata"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Goodbye frizz, hello smooth. With Kebelo ADVANTAGE smoothing system you will get 100 days of frizz-free, easy to manage, healthy, shiny, smooth hair. The Kebelo ADVANTAGE smoothing treatment takes three simple steps; cleanse, treat and maintain. Using Kebelo pioneered carbocysteine hair technology that allow the treatment to support, strengthen and revitalise each strand from within, weak, dull, frizzy, unhealthy hair is transformed to beautiful smooth locks filled with bounce and volume."; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.paulkemphairdressing.com/images/kebelo/kebelo_meta.jpg"; ?>
+<div id="kebeloMain">
+
+	<div id="advantage">
+	
+	<div id="kebelo-competition">
+	<h3>Hi <?php echo ucfirst($model->first_name); ?>, <br>how would you like <br>100 days of<br>frizz-free hair?</h3>
+	<p>You recently entered our Kebelo competition and you have been selected to have the Kebelo treatment at a special price!</p>
+	<div class="kebelo-offer"><p>40% off</p> <p>the Kebelo Advantage Smoothing treatment</p>
+	<p>Including aftercare products - Until the end of November.</p>
+	<small>(Not with any other offer)</small></div>
+	
+	<p>Goodbye frizz, hello smooth. With Kebelo ADVANTAGE smoothing system you will get 100 days of frizz-free, easy to manage, healthy, shiny, smooth hair.</p>
+	
+	<p class="offer"><strong>Call 01925 242960</strong></p>
+	<p class="offer">to book your appointment</p>
+	<p class="offer">Quote Offer Code: <strong>KEB60</strong></p>
+	</div> <!--end kebelo-competition copy-->
+	<div id="three_advantages">
+	<h3>3 advantages of ADVANTAGE</h3>
+	<p>Kebelo Spent 18 months creating the first ever long term smoothing system and, in the process, gained a reputation for innovation, safety and creativity. Now they have pioneered a new treatment which has three distinctive benefits not found anywhere else.</p>
+	<ol>
+	<li><strong>TIME:</strong> It only takes one hour to complete. Thanks to the new dispersion technology, the entire application process is quicker and more enjoyable.</li>
+	<li><strong>COLOUR:</strong> We can apply colour directly after your Kebelo System Treatment. As it gets to work laying a foundation deep inside the hair, it gives your new colour a vibrancy like never before.</li>
+	<li><strong>AFFORDABLE:</strong> You will get shiny, frizz free, easy-to-manage hair from just &pound;99, and that includes over &pound;25 worth of after-care. It amounts to just &pound;1 a day to keep the frizz away!</li>
+	</ol>
+	</div> <!--end 3advantages-->
+	</div> <!--end advantage-->
+</div> <!--end kebeloMain-->
