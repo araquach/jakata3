dcd4a92492f39470f2cab1f7277d854c1f54df63
Araquach <adam@contra-design.com>
2015-12-02 16:45:03 +0000

    Christmas Raffle marketing

--- protected/views/site/pages/competition.php
+++ protected/views/site/pages/competition.php
@@ -4,8 +4,8 @@ $this->pageTitle=Yii::app()->name . 'Competitions';
 ?>
 
 <?php Yii::app()->facebook->ogTags['og:title'] = "The Jakata Christmas Give-away"; ?>
-<?php Yii::app()->facebook->ogTags['og:description'] = "Hundreds of poundsworth of prizes to be won! GHD Styling tools, meal for 4 at Grill on the Square plus Colour & Cut package with your favourite stylist!"; ?>
-<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/competition/christmas_raffle_meta.jpg"; ?>
+<?php Yii::app()->facebook->ogTags['og:description'] = "Hundreds of poundsworth of prizes to be won! Meal for 4 at Las Ramblas PLUS £100 Jakata voucher, Limited Edition GHD Stylers & Hairdryer, PLUS 30 runners up prizes! "; ?>
+<?php Yii::app()->facebook->ogTags['og:image'] =  "http://www.jakatasalon.co.uk/images/competition/christmas_raffle_15_meta.jpg"; ?>
 
 
 <section id="competition">
@@ -16,30 +16,43 @@ $this->pageTitle=Yii::app()->name . 'Competitions';
 <div id="prizes">
 	<div class="prize">
 		<h3>Top Prize</h3>
-		<p>The ultimate GHD styling tool collection</p>
 		<ul>
-			<li>A Set of GHD Gold series Irons - worth £120</li>
-			<li>A GHD Air hairdryer - worth £99</li>
-			<li>A set of GHD Curve styling tongues - worth £110)</li>
-			<li>Plus a selection of GHD brushes, products and other goodies</li>
+			<li>A meal for 4 at Las Ramblas
+			  Wine &amp; Tapas Restaurant</li>
+			<li>A £100 Jakata Voucher</li>
+			<li>A bottle of Veuve Clicquot</li>	
 		</ul>
-		<p>A prize worth over £400!!</p>
+		<p>A total prize value of £400!</p>
 	</div> <!--.prize-->
 	
 	<div class="prize">
 		<h3>Second Prize</h3>
-		<p>A meal for four at Grill on the Square</p>
-		<p>Grill on the Square is one of Warrington’s finest restaurants. You can expect amazing food, great cocktails and impeccable service.</p>
-		<p>The prize consists of: A 3 course meal complete with a bottle of Veuve Clicquot for a party of four people</p>
-		<p>Worth over £250!!!</p>
+		<ul>
+			<li>Limited Edition 
+			  GHD Stylers &amp; Hairdryer</li>
+			<li>A GHD Styling wand</li>
+			<li>A £50 Jakata Voucher</li>
+			<li>A bottle of Veuve Clicquot</li>	
+		</ul>
+		<p>A total prize value of over £300!</p>
 	
 	</div> <!--.prize-->
 	
 	<div class="prize">
-		<h3>Third Prize</h3>
-		<p>Colour, Cut &amp; product package</p>
-		<p>A FREE Colour &amp; Cut with your chosen stylist plus a hamper of  salon professional products.</p>
-		<p>Worth over £100!!</p>
+		<h3>PLUS 30 RUNNERS UP PRIZES</h3>
+		<ul>
+			<li>Treatments</li>
+			<li>Styling Wands</li>
+			<li>Colours</li>
+			<li>Cuts</li>
+			<li>Blowdrys</li>
+			<li>Vouchers</li>
+			<li>Champagne</li>
+			<li>Kebelo's</li>
+			<li><strong>Plus loads of other prizes!</strong></li>
+			
+		
+		</ul>
 	</div> <!--.prize-->
 
 </div> <!--#prizes-->
@@ -47,7 +60,7 @@ $this->pageTitle=Yii::app()->name . 'Competitions';
 <div id="prize_entry">
 <p>To enter simply purchase prize draw tickets from salon reception. Tickets are £1 each and you can buy as many as you like!<br>
 For every Christmas Gift pack purchased you will also receive a FREE prize draw ticket.</p>
-<p>The winner will be drawn on Monday 22nd December - in time for you to pick up your prize before Christmas</p>
+<p>The winner will be drawn on Thursday 22nd December - in time for you to pick up your prize before Christmas</p>
 </div> <!--prize_entry-->
 
 <div id="fb_like">
